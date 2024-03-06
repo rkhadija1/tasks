e0b603fa04cf1be1243d4033ec9816e6528ebb65
Khadija <khadija@udel.edu>
2024-03-05 21:19:05 -0500

    finished task 8

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,38 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<string>("🌸");
+    function Alphabet(): void {
+        if (holiday === "🌸") {
+            setHoliday("🎃");
+        } else if (holiday === "🎃") {
+            setHoliday("🐏");
+        } else if (holiday === "🐏") {
+            setHoliday("🎄");
+        } else if (holiday === "🎄") {
+            setHoliday("🌝");
+        } else if (holiday === "🌝") {
+            setHoliday("🌸");
+        }
+    }
+    function Year(): void {
+        if (holiday === "🌸") {
+            setHoliday("🌝");
+        } else if (holiday === "🌝") {
+            setHoliday("🐏");
+        } else if (holiday === "🐏") {
+            setHoliday("🎃");
+        } else if (holiday === "🎃") {
+            setHoliday("🎄");
+        } else if (holiday === "🎄") {
+            setHoliday("🌸");
+        }
+    }
+    return (
+        <div>
+            <span>Holiday: {holiday}</span>
+            <Button onClick={Alphabet}>Advance by Alphabet</Button>
+            <Button onClick={Year}>Advance by Year</Button>
+        </div>
+    );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -1,4 +1,3 @@
-import { spawn } from "child_process";
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
